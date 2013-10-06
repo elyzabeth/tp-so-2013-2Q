ba5a1fa936ac86107130364f3c0158bb30d1e715
elyzabeth <elizabeth.sosa@gmail.com>
2013-10-06 16:14:46 -0300

    Me faltaron los cambios en nivel

--- nivel/NivelMain.c
+++ nivel/NivelMain.c
@@ -1,11 +1,11 @@
 /*
- * nivel.c
+ * NivelMain.c
  *
  *  Created on: 22/09/2013
  *      Author: elyzabeth
  */
 
-#include "Nivel.h"
+#include "NivelMain.h"
 
 
 int main (int argc, char**argv) {
@@ -14,23 +14,24 @@ int main (int argc, char**argv) {
 	if (argc > 1 && strcmp(argv[1], "-test")==0)
 		return correrTest();
 
-	inicializar ();
+	// Registro signal and signal handler
+	signal(SIGINT, signal_callback_handler);
+	signal(SIGQUIT, signal_callback_handler);
+	signal(SIGUSR1, signal_callback_handler);
+	signal(SIGTERM, signal_callback_handler);
+
+	inicializarNivel();
 	principal ();
-	finalizar ();
+	//while(1);
+	finalizarNivel();
 
 	return EXIT_SUCCESS;
 }
 
-int correrTest() {
-	return suiteNivel();
-}
+
 
 void principal () {
-	inicializar();
 
-	t_list* items = list_create();
-
-		int rows, cols;
 		int q, p;
 
 		int x = 1;
@@ -39,24 +40,20 @@ void principal () {
 		int ex1 = 10, ey1 = 14;
 		int ex2 = 20, ey2 = 3;
 
-		nivel_gui_inicializar();
+		p = MAXCOLS;
+		q = MAXROWS;
 
-	    nivel_gui_get_area_nivel(&rows, &cols);
+		CrearPersonaje(GUIITEMS, '@', p, q);
+		CrearPersonaje(GUIITEMS, '#', x, y);
 
-		p = cols;
-		q = rows;
+//		CrearEnemigo(GUIITEMS, '1', ex1, ey1);
+//		CrearEnemigo(GUIITEMS, '2', ex2, ey2);
 
-		CrearPersonaje(items, '@', p, q);
-		CrearPersonaje(items, '#', x, y);
+//		CrearCaja(GUIITEMS, 'H', 26, 10, 5);
+//		CrearCaja(GUIITEMS, 'M', 8, 15, 3);
+//		CrearCaja(GUIITEMS, 'F', 19, 9, 2);
 
-		CrearEnemigo(items, '1', ex1, ey1);
-		CrearEnemigo(items, '2', ex2, ey2);
-
-		CrearCaja(items, 'H', 26, 10, 5);
-		CrearCaja(items, 'M', 8, 15, 3);
-		CrearCaja(items, 'F', 19, 9, 2);
-
-		nivel_gui_dibujar(items, "Test Chamber 04");
+		nivel_gui_dibujar(GUIITEMS, NOMBRENIVEL);
 
 		while ( 1 ) {
 			int key = getch();
@@ -70,7 +67,7 @@ void principal () {
 				break;
 
 				case KEY_DOWN:
-					if (y < rows) {
+					if (y < MAXROWS) {
 						y++;
 					}
 				break;
@@ -81,7 +78,7 @@ void principal () {
 					}
 				break;
 				case KEY_RIGHT:
-					if (x < cols) {
+					if (x < MAXCOLS) {
 						x++;
 					}
 				break;
@@ -94,7 +91,7 @@ void principal () {
 
 				case 's':
 				case 'S':
-					if (q < rows) {
+					if (q < MAXROWS) {
 						q++;
 					}
 				break;
@@ -107,85 +104,56 @@ void principal () {
 				break;
 				case 'D':
 				case 'd':
-					if (p < cols) {
+					if (p < MAXCOLS) {
 						p++;
 					}
 				break;
 				case 'Q':
 				case 'q':
-					nivel_gui_terminar();
-					exit(0);
+					//nivel_gui_terminar();
+					//exit(0);
 				break;
 			}
 
 
-			rnd(&ex1, cols);
-			rnd(&ey1, rows);
-			rnd(&ex2, cols);
-			rnd(&ey2, rows);
-			MoverPersonaje(items, '1', ex1, ey1 );
-			MoverPersonaje(items, '2', ex2, ey2 );
+			rnd(&ex1, MAXCOLS);
+			rnd(&ey1, MAXROWS);
+			rnd(&ex2, MAXCOLS);
+			rnd(&ey2, MAXROWS);
+			MoverPersonaje(GUIITEMS, '1', ex1, ey1 );
+			MoverPersonaje(GUIITEMS, '2', ex2, ey2 );
 
-			MoverPersonaje(items, '@', p, q);
-			MoverPersonaje(items, '#', x, y);
+			MoverPersonaje(GUIITEMS, '@', p, q);
+			MoverPersonaje(GUIITEMS, '#', x, y);
 
 			if (   ((p == 26) && (q == 10)) || ((x == 26) && (y == 10)) ) {
-				restarRecurso(items, 'H');
+				restarRecurso(GUIITEMS, 'H');
 			}
 
 			if (   ((p == 19) && (q == 9)) || ((x == 19) && (y == 9)) ) {
-				restarRecurso(items, 'F');
+				restarRecurso(GUIITEMS, 'F');
 			}
 
 			if (   ((p == 8) && (q == 15)) || ((x == 8) && (y == 15)) ) {
-				restarRecurso(items, 'M');
+				restarRecurso(GUIITEMS, 'M');
 			}
 
 			if((p == x) && (q == y)) {
-				BorrarItem(items, '#'); //si chocan, borramos uno (!)
+				BorrarItem(GUIITEMS, '#'); //si chocan, borramos uno (!)
 			}
 
-			nivel_gui_dibujar(items, "Test Chamber 04");
+			nivel_gui_dibujar(GUIITEMS, NOMBRENIVEL);
+
+			if (key=='q' || key=='Q')
+				break;
 		}
 
-		BorrarItem(items, '#');
-		BorrarItem(items, '@');
-
-		BorrarItem(items, '1');
-		BorrarItem(items, '2');
-
-		BorrarItem(items, 'H');
-		BorrarItem(items, 'M');
-		BorrarItem(items, 'F');
-
-		nivel_gui_terminar();
-
-	finalizar();
-}
-
-
-void inicializar () {
-	levantarArchivoConfiguracionNivel();
-	LOGGER = log_create(configNivelLogPath(), "NIVEL", configNivelLogConsola(), configNivelLogNivel() );
-	strncpy(NOMBRENIVEL, configNivelNombre(), 20);
-	log_info(LOGGER, "INICIALIZANDO NIVEL '%s' ", NOMBRENIVEL);
+		return;
 
 }
 
-void finalizar () {
-	log_info(LOGGER, "FINALIZANDO NIVEL '%s'", NOMBRENIVEL);
-	destruirConfigNivel();
-	log_destroy(LOGGER);
-}
 
-/*
- * @NAME: rnd
- * @DESC: Modifica el numero en +1,0,-1, sin pasarse del maximo dado
- */
-void rnd(int *x, int max){
-	*x += (rand() % 3) - 1;
-	*x = (*x<max) ? *x : max-1;
-	*x = (*x>0) ? *x : 1;
-}
+
+
 
 
--- nivel/NivelMain.h
+++ nivel/NivelMain.h
@@ -1,34 +1,19 @@
 /*
- * nivel.h
+ * NivelMain.h
  *
  *  Created on: 22/09/2013
  *      Author: elyzabeth
  */
 
-#ifndef NIVEL_H_
-#define NIVEL_H_
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <curses.h>
-
-#include "commons/log.h"
-#include <commons/collections/list.h>
-
-#include "cunitTest/cUnitSuiteNivel.h"
-#include "config/configNivel.h"
-#include "tad_items.h"
+#ifndef NIVELMAIN_H_
+#define NIVELMAIN_H_
 
 
-t_log* LOGGER;
-char NOMBRENIVEL[20+1];
 
-int correrTest();
-void principal ();
+#include "funcionesNivel.h"
 
-void inicializar ();
-void finalizar ();
 
-void rnd(int *x, int max);
 
-#endif /* NIVEL_H_ */
+
+
+#endif /* NIVELMAIN_H_ */
