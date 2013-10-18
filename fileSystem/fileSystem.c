83816f03c86453dc38c9266b9624315691966f48
elyzabeth <elizabeth.sosa@gmail.com>
2013-10-18 17:47:08 -0300

    Agrego proyecto fileSystem.

---
+++ fileSystem/fileSystem.c
@@ -0,0 +1,13 @@
+/*
+ * fileSystem.c
+ *
+ *  Created on: 18/10/2013
+ *      Author: arwen
+ */
+
+#include "fileSystem.h"
+
+int main (int argc, char**argv) {
+
+	return EXIT_SUCCESS;
+}
---
+++ fileSystem/fileSystem.h
@@ -0,0 +1,14 @@
+/*
+ * fileSystem.h
+ *
+ *  Created on: 18/10/2013
+ *      Author: arwen
+ */
+
+#ifndef FILESYSTEM_H_
+#define FILESYSTEM_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#endif /* FILESYSTEM_H_ */
